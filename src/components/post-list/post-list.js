7ce5c866ec4a66433a38d59ec08c8bbecc17000f
Safarov <elkhan.safarov10@yandex.com>
2020-02-12 22:48:50 +0500

    Удаление и добавление элементов

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import PostListItem from '../post-list-item/post-list-item'
+import { ListGroup } from 'reactstrap';
 import './post-list.css'
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
     const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
         return (
-            <li key={item.id} className='list-group-item'>
+            <li key={id} className='list-group-item'>
                 <PostListItem
-                    label={item.label} 
-                    important={item.important} />
+                    {...itemProps} 
+                    onDelete={() => onDelete(id)}/>
             </li>
         )
     })
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
